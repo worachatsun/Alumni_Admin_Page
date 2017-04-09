1b0514ddeda09d919b01407a9086bca8bf82f50d
worachatsun <worachat.sun@mail.kmutt.ac.th>
2017-04-09 20:58:15 +0700

    add stater component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hey guys and ladies!!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
